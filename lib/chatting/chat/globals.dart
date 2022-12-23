f6ab28f585e7c1da1500c1622b90c1559a4e3f31
myHobbyBest <117243771+myHobbyBest@users.noreply.github.com>
2022-12-23 12:35:56 +0900

    Add files via upload

---
+++ lib/chatting/chat/globals.dart
@@ -0,0 +1,11 @@
+library my_prj.globals;
+
+var isFirstChat=true ;
+var isSecondChat=false ;
+var isSignUpUser=false ;
+var isChatDoc=false ;
+Map userData={};
+
+var userIdData;
+int count=0;
+int cCount=0;
